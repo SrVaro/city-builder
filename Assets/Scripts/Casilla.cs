74205cd19ccf65462a6b24c629264d10c97f9bcc
Varo <sr.varo07@gmail.com>
2018-11-10 11:32:06 +0100

    estructura clases 0.1

---
+++ Assets/Scripts/Casilla.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Casilla : MonoBehaviour {
+
+    private int posX;
+    private int posY;
+
+    public Casilla(int  posX, int posY)
+    {
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+	// Use this for initialization
+	void Start () {
+
+  
+	}
+    	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
