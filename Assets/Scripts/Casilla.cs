1379c3310e6919bf3212e795b0e5aeaba38b3d6f
Varo <sr.varo07@gmail.com>
2018-11-10 20:01:30 +0100

    PUTA SELECCION DE FICHAS

--- Assets/Scripts/Casilla.cs
+++ Assets/Scripts/Casilla.cs
@@ -7,14 +7,25 @@ public class Casilla : MonoBehaviour {
     private float posX;
     private float posZ;
 
+    private GameObject ficha;
+
     public bool ocupada = false;
+
+    private Material mat;
+
+    public static bool pulsada;
+
+    private Tablero tablero;
+
+    private MeshRenderer renderer;
 
     // Use this for initialization
     void Awake () {
 
-
         this.posX = transform.position.x;
         this.posZ = transform.position.z;
+
+        renderer = GetComponentInChildren<MeshRenderer>();
 
     }
     	
@@ -25,28 +36,60 @@ public class Casilla : MonoBehaviour {
 
     public void crearFicha(GameObject ficha)
     {
-        Debug.Log(posX + ", " + posZ);
-
-        Instantiate(ficha, new Vector3(posX, 2, posZ), transform.rotation);
-
+        this.ficha  = Instantiate(ficha, new Vector3(posX, 1f, posZ), transform.rotation);
+    
         ocupada = true;
+    }
 
+    public GameObject getFicha()
+    {
+        return ficha;
     }
 
     void OnMouseDown()
     {
-        
+        if(ocupada == false)
+        {
+            mat = renderer.material;
+
+            mat.color = Color.green;
+
+            tablero.crearFichaEnCasillaSeleccionada(this);
+           
+        }
+        else
+        {
+            mat = renderer.material;
+
+            mat.color = Color.red;
+
+        }
+
     }
 
+    void OnMouseUp()
+    {
 
-    
+        mat = renderer.material;
+
+        mat.color = Color.white;
+    }
+
 
     public bool estaOcupado()
     {
         return ocupada;
     }
 
+    public void setTablero(Tablero tablero)
+    {
+        this.tablero = tablero;
+    }
     
+
+
+    
+
 
 
 }
--- Assets/Scripts/Tablero.cs
+++ Assets/Scripts/Tablero.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Tablero : MonoBehaviour {
 
-    private int contaminacion;
+    private int fichaSeleccionada = 0;
+
+    private int limitecontaminacion;
 
     public GameObject celda;
+
+    public GameObject edificio;
 
     public GameObject parque;
 
@@ -17,6 +21,7 @@ public class Tablero : MonoBehaviour {
     public int numFil = 0;
 
     private Casilla[,] matrizCasillas;
+
 
 
     // Use this for initialization
@@ -30,19 +35,63 @@ public class Tablero : MonoBehaviour {
             {
                 Casilla casilla = Instantiate(celda, new Vector3(i * 2f, 0, j * 2f), celda.transform.rotation).GetComponent<Casilla>();
 
+                casilla.setTablero(this);
+
                 matrizCasillas[i, j] = casilla;
 
             }
         }
 
 
-        matrizCasillas[0, 0].crearFicha(industria);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
 
 
     }
 
-    // Update is called once per frame
-    void Update () {
-		
-	}
+    public string fichaPosterior(int posMatrizX, int posMatrizZ)
+    {   
+        return null;
+    }
+
+
+
+    public void seleccionarFicha(int tipoFicha)
+    {
+        this.fichaSeleccionada = tipoFicha;
+
+    }
+
+   public void crearFichaEnCasillaSeleccionada(Casilla casilla)
+    {
+        switch (fichaSeleccionada)
+        {
+            case 0:
+
+                Debug.Log("No hay ficha seleccionada");
+                break;
+
+            case 1:
+
+                casilla.crearFicha(edificio);
+                break;
+
+            case 2:
+
+                casilla.crearFicha(industria);
+                break;
+
+            case 3:
+
+                casilla.crearFicha(parque);
+                break;
+
+        }
+
+    }
+
 }
